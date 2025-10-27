054528d2d2c98936444bebb159a217f923bb2e7a
Dogan Gunes <dogangns.98@gmail.com>
2025-10-28 00:00:48 +0300

    .

---
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
@@ -0,0 +1,37 @@
+﻿using eCommerce.SharedLibrary.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.SharedLibrary.DependencyInjection
+{
+    public static class SharedServiceContainer
+    {
+        public static IServiceCollection AddSharedServices<TContext>(this IServiceCollection services, IConfiguration config, string fileName) where TContext : DbContext
+        {
+            services.AddDbContext<TContext>(option => option.UseSqlServer(config.GetConnectionString("eCommerceConnection"), sqlserverOption => sqlserverOption.EnableRetryOnFailure()));
+
+            Log.Logger = new LoggerConfiguration().MinimumLevel.Information().WriteTo.Debug().WriteTo.Console().WriteTo.File(path: $"{fileName}-text", 
+                                        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information, outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {message:lj}{NewLine}{Exception}", 
+                                        rollingInterval: RollingInterval.Day).CreateBootstrapLogger();
+
+            JWTAuthenticationScheme.AddJWTAuthenticationScheme(services, config);
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSharedPolicies(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<GlobalException>();
+            app.UseMiddleware<ListenToOnlyApiGateway>();
+            return app;
+        }
+    }
+}
--- DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/Middleware/ListenToOnlyApiGateway.cs
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/Middleware/ListenToOnlyApiGateway.cs
@@ -1,24 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Microsoft.AspNetCore.Http;  
 namespace eCommerce.SharedLibrary.Middleware
 {
     public class ListenToOnlyApiGateway(RequestDelegate next)
     {
-        public async Task InvokeAsync(HttpContent context)
+        public async Task InvokeAsync(Microsoft.AspNetCore.Http.HttpContext context)
         {
             // Extract specific header from the request
             var signedHeader = context.Request.Headers["Api-Gateway"];
 
             // NULL means, the request is not coming from the Api Gateway
-
-
-
-
+            if (signedHeader.FirstOrDefault() is null) 
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Sorry, service is unavaible");
+                return;
+            }
+            else
+            {
+                await next(context);
+            } 
         }
 
     }
